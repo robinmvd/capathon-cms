75a8b79a2297c646e3405f89d4b53e3944e8165b
Mynorka Daza <mynorkadaza@gmail.com>
2025-03-24 13:46:20 +0100

    test:Add test for Home component

--- __tests__/page.test.js
+++ __tests__/page.test.js
@@ -2,4 +2,7 @@ import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import Home from '../src/app/page'
 
-//create a test to check if the Home component renders correctly
+test('renders Home component', () => {
+  const {container} = render(<Home />);
+  expect(container).toBeInTheDocument();
+})
